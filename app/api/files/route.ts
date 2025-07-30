1a67f690999866bcb2b6dee1bb68238d3d60312c
Gerard Dunn <gerard.dunn.92@gmail.com>
2025-07-30 14:24:28 +1200

    added comments for reference in file route

--- app/api/files/route.ts
+++ app/api/files/route.ts
@@ -7,10 +7,13 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
   try {
+    // Authenticate the user
     const { userId } = await auth()
     if (!userId) {
       return NextResponse.json({ error: ' Unauthorized' }, { status: 401 })
     }
+
+    // get query parameters
     const searchParams = request.nextUrl.searchParams
     const queryUserId = searchParams.get('userId')
     const parentId = searchParams.get('parentId')
@@ -19,15 +22,16 @@ export async function GET(request: NextRequest) {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
     }
 
-    // fetch files from database
-
+    // fetch files from database based on userId and parentId
     let userFiles
     if (parentId) {
+      //fetch the files within a specific parent folder
       userFiles = await db
         .select()
         .from(files)
         .where(and(eq(files.userID, userId), eq(files.parentID, parentId)))
     } else {
+      // fetch the files in the root folder
       userFiles = await db
         .select()
         .from(files)
